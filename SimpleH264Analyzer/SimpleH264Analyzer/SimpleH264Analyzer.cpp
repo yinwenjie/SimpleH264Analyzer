21100d4efc0c7b920a3af287b368638d413b0370
yinwenjie <eternal.ywj@gmail.com>
2016-07-27 10:14:25 +0800

    删除原有的NAL提取部分

--- SimpleH264Analyzer/SimpleH264Analyzer/SimpleH264Analyzer.cpp
+++ SimpleH264Analyzer/SimpleH264Analyzer/SimpleH264Analyzer.cpp
@@ -8,8 +8,6 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	CStreamFile h264stream(_T("test.h264"));
 
-	h264stream.Parse_h264_bitstream();
-
 	return 0;
 }
 
--- SimpleH264Analyzer/SimpleH264Analyzer/Stream.h
+++ SimpleH264Analyzer/SimpleH264Analyzer/Stream.h
@@ -8,19 +8,11 @@ class CStreamFile
 public:
 	CStreamFile(TCHAR *fileName);
 	~CStreamFile();
-	
-	//Open API
-	int Parse_h264_bitstream();
 
 private:
 	FILE	*m_pFile_In;
 	TCHAR	*m_pFileName;
 	std::vector<uint8> m_NalVec;
-
-	//Private methods
-	int find_first_NAL_unit();
-	int extract_nal_unit();
-	int peek_bytes(int n);
 };
 
 #endif
