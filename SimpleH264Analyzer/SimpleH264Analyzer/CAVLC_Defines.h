004594575bbe5398bbf549ab1652cb3fea304205
yinwenjie <eternal.ywj@gmail.com>
2017-02-09 21:32:39 +0800

    漏提交的文件

---
+++ SimpleH264Analyzer/SimpleH264Analyzer/CAVLC_Defines.h
@@ -0,0 +1,9 @@
+#ifndef _CAVLC_DEFINES_H_
+#define _CAVLC_DEFINES_H_
+
+// CAVLC
+#define LUMA              0
+#define LUMA_INTRA16x16DC 1
+#define LUMA_INTRA16x16AC 2
+
+#endif
---
+++ SimpleH264Analyzer/SimpleH264Analyzer/Macroblock_Defines.h
@@ -0,0 +1,13 @@
+#ifndef _MACROBLOCK_DEFINES_H_
+#define _MACROBLOCK_DEFINES_H_
+
+// MB_Type
+#define P8x8    8
+#define I4MB    9
+#define I16MB   10
+#define IBLOCK  11
+#define SI4MB   12
+#define MAXMODE 13
+#define IPCM    14
+
+#endif
